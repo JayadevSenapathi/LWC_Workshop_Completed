285c10159389b1f97943ce02772b3f507fdc72fe
Jayadev Senapathi Kota <kvedayaj3@gmail.com>
2022-05-11 06:17:50 +0530

    Exercise 2-9 Contact Selected

--- force-app/Exercises/main/default/lwc/studentTiles/studentTiles.js
+++ force-app/Exercises/main/default/lwc/studentTiles/studentTiles.js
@@ -2,4 +2,10 @@ import { api, LightningElement } from 'lwc';
 
 export default class StudentTiles extends LightningElement {
     @api studentList = [];
+
+    selectedStudentId = '';
+
+    handleStudentSelected(event) {
+        this.selectedStudentId = event.detail.studentId;
+    }
 }
