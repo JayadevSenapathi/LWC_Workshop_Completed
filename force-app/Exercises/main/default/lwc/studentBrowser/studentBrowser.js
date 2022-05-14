c22cfb40ba6b975788ebcb5c3c119754bb19df39
Jayadev Senapathi Kota <kvedayaj3@gmail.com>
2022-05-15 00:37:02 +0530

    Exercise 5-2 Completed

--- force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
+++ force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
@@ -63,4 +63,9 @@ export default class StudentBrowser extends NavigationMixin(LightningElement) {
             }
         });
     }
+
+    handleRowClick(event) {
+        const studentId = event.detail.pk;
+        this.updateSelectedStudent(studentId);
+    }
 }
